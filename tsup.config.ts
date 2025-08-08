216a7bd823f0d7846cf013647c23396ddab23976
Devan Aditiya <50104357+adwerygaming@users.noreply.github.com>
2025-08-08 20:56:17 +0700

    A# fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/**/*.ts'],
+    entry: ['src/**/*.ts', "!src/**/node_modules/**"],
     outDir: 'dist',
     format: ['esm'],
     sourcemap: false,
     dts: false,
-    clean: true
+    clean: true,
+    bundle: false,
+    splitting: false
 });
